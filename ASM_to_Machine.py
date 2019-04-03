6ee8be57ff9641de3e5a7e8853cdf97143152d00
t17rous <thomas-rousseau44@laposte.net>
2019-04-03 11:25:24 +0200

    Specifications ASMtoMachine

--- ASM_to_Machine.py
+++ ASM_to_Machine.py
@@ -1,1 +1,10 @@
 #Script de conversion d'une entrée ASM à un code exécutable par le processeur ternaire
+
+#CdC - Lecture de fichiers texte .truitep
+# format ASM : 0: OPT OPERAND OPERAND
+#              1: OPT OPERAND OPERAND
+#              3: OPT OPERAND OPERAND
+#              ....
+# Fichier d'architecture (JSON)
+#Sort un dictionnaire JSON de mots TCD mémoire pour clé adresse mémoire séquentielle affecte un mot
+
