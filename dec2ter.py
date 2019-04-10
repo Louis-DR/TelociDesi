81fd7b9b676d2b301708dee9a04268d482aa42ff
Louis Duret-Robert <louisduret@gmail.com>
2019-04-10 11:05:28 +0200

    exemple pour Thomas

--- dec2ter.py
+++ dec2ter.py
@@ -1,5 +1,3 @@
 def dec2ter(a):
     if a//3==0 : return [a%3]
     else : return dec2ter(a//3)+[a%3]
-
-print(dec2ter(5))
--- ter2dec.py
+++ ter2dec.py
@@ -1,13 +1,12 @@
 def ter2dec(a):
     res = 0
-    for k in range(len(a)-1,-1,-1):
-        print("{}*3^{}".format(a[k],k))
-        res += a[k]*3^k
+    for k in range(len(a)):
+        res += a[-k]*3**(len(a)-k-1)
     return res
 
-print(ter2dec([0]))
-print(ter2dec([1]))
-print(ter2dec([2]))
-print(ter2dec([1,0]))
-print(ter2dec([1,1]))
-print(ter2dec([1,2]))
+print(ter2dec([2,2,0,2,0,0]))
+print(ter2dec([2,1,2,2,1,2]))
+print(ter2dec([1,1,2,1,1,1]))
+print(ter2dec([2,0,0,1,1,1]))
+print(ter2dec([1,1,1,1,1,1]))
+print(ter2dec([1,1,2,2,0,0]))
