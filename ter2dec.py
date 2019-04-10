720ac677ca37d50add729e823a11fd769255b8ce
Louis Duret-Robert <louisduret@gmail.com>
2019-04-10 11:15:25 +0200

    remove pycache

--- ter2dec.py
+++ ter2dec.py
@@ -3,10 +3,3 @@ def ter2dec(a):
     for k in range(len(a)):
         res += a[-k]*3**(len(a)-k-1)
     return res
-
-print(ter2dec([2,2,0,2,0,0]))
-print(ter2dec([2,1,2,2,1,2]))
-print(ter2dec([1,1,2,1,1,1]))
-print(ter2dec([2,0,0,1,1,1]))
-print(ter2dec([1,1,1,1,1,1]))
-print(ter2dec([1,1,2,2,0,0]))
