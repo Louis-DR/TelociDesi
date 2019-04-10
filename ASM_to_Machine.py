2b37a3fbee54f85302a689c58a61a55fa20c6f37
t17rous <thomas-rousseau44@laposte.net>
2019-04-10 11:10:09 +0200

    LoadFiles

--- ASM_to_Machine.py
+++ ASM_to_Machine.py
@@ -9,19 +9,40 @@
 #Sort un dictionnaire JSON de mots TCD (ternaire codé décimal) mémoire pour clé adresse mémoire séquentielle affecte un mot
 
 import json
+from tkinter import *
 
 #Load architecture specifications
-farchi = open("architecture.truitea","r")
-archi =  json.load (farchi)
-farchi.close();
+def loadArchi():
+    filename = askopenfilename(title="Ouvrir un fichier architecture",filetypes=[('truitea files','*.truitea'),('all files','*.*')])
+    farchi = open(filename,"r")
+    archi =  json.load (farchi)
+    farchi.close()
+    return archi
 
 #Load assembly file
-#fspec = open("spec.truitep","r")
-#assembly = fspec.readlines()
-#fspec.close()
+def loadASM():
+    filenameASM = askopenfilename(title="Ouvrir votre document",filetypes=[('txt files','.txt'),('all files','.*')])
+    fspec = open(filenameASM,"r")
+    assembly = fspec.readlines()
+    fspec.close()
+
+fenetre = Tk()
+
+#Architecture frames
+Frame1  = Frame(fenetre)
+Frame1.pack(side = LEFT, padx=30, pady= 30)
 
 
+#Frame1
+labelASM = Label(Frame1, text = "Fichier assembleur").pack()
+bouton=Button(Frame1, text="Charger", command=loadArchi).pack()
 
-print( archi ["architecture"])
+fenetre.mainloop()
 
-fspec.close();
+#Première ligne de assembly : informations importantes
+
+
+#print( archi ["architecture"])
+
+#fspec.close();
+
