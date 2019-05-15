d6943374b766e8a6e93e26e95de819f9ed2ab082
Alexandre Georjon <alexandre.georjon@gmail.com>
2019-05-15 10:48:13 +0200

    AddInvGate

--- TelociDesi.py
+++ TelociDesi.py
@@ -20,7 +20,7 @@ import random
 
 #region [red] CONFIGURATION
 
-TKINTER_SCALING = 1.0
+TKINTER_SCALING = 0.4
 GRID_WIDTH = 50
 GRID_HEIGHT = 50
 GRID_UNIT = TKINTER_SCALING*40
@@ -1889,8 +1889,21 @@ def importMemory():
 menuBar = Menu(root)
 #root['menu'] = menuBar
 
-def hello():
-    print("bite")
+def invGate(sel_id):
+    print(gates[sel_id])
+    if(gates[sel_id]["gate"][0]!="N"):
+        gates[sel_id]["gate"]="N"+gates[sel_id]["gate"]
+    else:
+        gates[sel_id]["gate"]=gates[sel_id]["gate"][1::]
+    drawAll()
+    print(gates[sel_id])
+
+def invGates(sel_ids):
+    for sel_id in sel_ids:
+        invGate(sel_id)
+    drawAll()
+    
+
 
 sousMenu = Menu(menuBar)
 menuBar.add_cascade(label='File', menu=sousMenu)
@@ -1900,11 +1913,13 @@ sousMenu.add_separator()
 sousMenu.add_command(label='New',font=(FONT_FAMILY, FONT_SIZE),command=blankCircuit)
 sousMenu.add_command(label='Exit',font=(FONT_FAMILY, FONT_SIZE),command=root.destroy)
 
+sousMenu2 = Menu(menuBar)
+menuBar.add_cascade(label='Tools', menu=sousMenu2)
+sousMenu2.add_command(label='Inv',font=(FONT_FAMILY, FONT_SIZE),command=lambda : invGates(selection))
+
 
 #sousMenu.add(label='Load', font=(FONT_FAMILY, FONT_SIZE) ))
 #sousMenu.add(label='Exit', font=(FONT_FAMILY, FONT_SIZE) ))
-#sousMenu.add(label='Bite', font=(FONT_FAMILY, FONT_SIZE) ))
-#sousMenu.add(label='Bite', font=(FONT_FAMILY, FONT_SIZE) ))
 
 root.config(menu=menuBar)
 #end region
