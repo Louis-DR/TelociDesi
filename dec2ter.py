0d0b9c51b69e5c5d7490394a3b23a43e5796a7e5
t17rouss <39629067+t17rouss@users.noreply.github.com>
2019-05-22 13:14:18 +0200

    Formatted ASM wordsize according to architecture

--- dec2ter.py
+++ dec2ter.py
@@ -7,3 +7,16 @@ def dec2terstr(list):
     for trit in list:
         res = res + str(trit)
     return res
+
+def dec2terXtrit(number, ntrits):
+    temp = dec2terstr(dec2ter(number))
+    res = ""
+    if (len(temp)>ntrits):
+        for i in range(ntrits):
+            res = res + "2"
+    elif (len(temp)>0):
+        res = temp
+        for j in range (len(temp),ntrits):
+            res = "0"+res
+    return res
+            
